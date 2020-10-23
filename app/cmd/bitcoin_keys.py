786f32e59c2492cb238917d849f66ad814f33fe8
Max Block <maxblock@pm.me>
2020-10-23 11:58:55 +0300

    Basic version of bitcoin-keys #2

--- app/cmd/bitcoin_keys.py
+++ app/cmd/bitcoin_keys.py
@@ -1,6 +1,27 @@
 import click
+from bip_utils import Bip39MnemonicGenerator, Bip39SeedGenerator, Bip39WordsNum, Bip44, Bip44Changes, Bip44Coins
 
 
 @click.command()
-def cli():
-    click.echo("it works")
+@click.option("-m", "--mnemonic")
+@click.option("-p", "--passphrase", default="")
+@click.option("-l", "--limit", default=5)
+@click.option("--print-private/--no-print-private", default=True)
+def cli(mnemonic: str, passphrase: str, limit: int, print_private: bool):
+    if not mnemonic:
+        mnemonic = Bip39MnemonicGenerator.FromWordsNumber(Bip39WordsNum.WORDS_NUM_24)
+    seed_bytes = Bip39SeedGenerator(mnemonic).Generate(passphrase)
+
+    click.echo(mnemonic + "\n")
+
+    bip_obj_mst = Bip44.FromSeed(seed_bytes, Bip44Coins.BITCOIN)
+    bip_obj_acc = bip_obj_mst.Purpose().Coin().Account(0)
+    bip_obj_chain = bip_obj_acc.Change(Bip44Changes.CHAIN_EXT)
+
+    # m/44'/0'/0'/0/i
+    for i in range(limit):
+        bip_obj_addr = bip_obj_chain.AddressIndex(i)
+        address = bip_obj_addr.PublicKey().ToAddress()
+        private = bip_obj_addr.PrivateKey().ToWif()
+        acc = f"{address} / {private}" if print_private else address
+        click.echo(acc)

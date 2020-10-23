33c0a0afdb02aeb572a5c3c0b6ae97a905c7fa32
Max Block <maxblock@pm.me>
2020-10-23 18:22:38 +0300

    Basic version of bitcoin-balances #3

--- app/cmd/btc_balance.py
+++ app/cmd/btc_balance.py
@@ -7,7 +7,8 @@ from app import blockcypher
 @click.argument("addresses", type=click.File())
 @click.option("--hide-empty/--no-hide-empty", default=False)
 @click.option("--satoshi/--no-satoshi", default=False)
-def cli(addresses, hide_empty: bool, satoshi: bool):
+@click.option("--round", "round_digits", default=8)
+def cli(addresses, hide_empty: bool, satoshi: bool, round_digits: int):
     addresses = addresses.read()
     for address in addresses.split("\n"):
         if not address:
@@ -15,5 +16,11 @@ def cli(addresses, hide_empty: bool, satoshi: bool):
         res = blockcypher.get_btc_balance(address)
         if res.is_ok() and res.value == 0 and hide_empty:
             continue
+        if res.is_err():
+            value = res.value
+        elif satoshi:
+            value = f"{res.value} sat"
+        else:
+            value = f"{round(res.value / 100_000_000, ndigits=round_digits)} btc"
 
-        # print(address, res)
+        click.echo(f"{address} / {value}")
